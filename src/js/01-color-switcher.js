e23ea0960b4d4cc5f1da177fbf61442f5090d445
Pumpon4yk <tt10392@gmail.com>
2022-12-24 13:33:52 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+    body: document.querySelector('body'),
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
 
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStart);
+refs.stopBtn.addEventListener('click', onStop);
+
+function onStart() {
+    refs.startBtn.setAttribute('disabled', 'disabled');
+    refs.stopBtn.removeAttribute('disabled');
+    timerId = setInterval(bodyBg, 1000);
+}
+
+function onStop() {
+    clearInterval(timerId);
+    refs.startBtn.removeAttribute('disabled');
+    refs.stopBtn.setAttribute('disabled', 'disabled');
+}
+
+function bodyBg() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
