6a1d140e2a72a7acaeec2665cf2a53dd52c9a1f7
Pumpon4yk <tt10392@gmail.com>
2022-12-27 13:01:59 +0100

    tasck 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form')
+
+form.addEventListener('submit', submitForm)
+
+function submitForm(e) {
+    e.preventDefault();
+    const { elements: { delay, step, amount } } = e.currentTarget;
+    let d = Number(delay.value);
+
+    for (let i = 1; i <= amount.value; i++) {
+        createPromise(i, d)
+            .then((yes) => {
+                Notify.success(yes)
+            })
+            .catch((no) => {
+                Notify.failure(no)
+            })
+        d += Number(step.value)
+    }
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+    const yes = `✅ Fulfilled promise ${position} in ${delay}ms`;
+    const no = `❌ Rejected promise ${position} in ${delay}ms`;
+
+    const promise = new Promise((res, rej) => {
+        const shouldResolve = Math.random() > 0.3;
+
+        setTimeout(() => {
+            if (shouldResolve) {
+                // Fulfill
+                res(yes);
+            } else {
+                // Reject
+                rej(no);
+            }
+        }, delay)
+    })
+    return promise;
 }
